1e5db7a8fd7e8e84f611efe7c1a81489c0a2a15c
DevobGit <levacancier2@gmail.com>
2024-04-15 21:41:22 +0200

    Ajout tournois

--- strat.py
+++ strat.py
@@ -13,6 +13,8 @@ import random
 # Fixer la graine aléatoire pour des résultats reproductibles
 random.seed(6436)
 
+# Définition des manches
+
 def prisoner_dilemma(player1, player2, round):
     outcomes = { # Créer un dictionnaire assignant chaque doublet d'action à son doublet de gains pour l'acteur
         (0, 0): (3, 3),
@@ -24,39 +26,63 @@ def prisoner_dilemma(player1, player2, round):
     return(outcomes[(player1.play(round), player2.play(round))])
 """ 
 Retourne les résultat/gains du tour en utilisant le prècèdent dictionnaire pour assigner des gains
-en fonction de l'action du joueur obtenur par sa méthode play, auquelle l'on indique le tour du tournoi
+en fonction de l'action du joueur obtenue par sa méthode play, auquelle l'on indique le tour du tournoi
 """
 
-def match(player1, player2, rounds):
-    score1, score2 = 0, 0 # Met les scores à 0
-    print("MATCH : Player", player1.name, "VERSUS Player", player2.name) # Print les infos du match
+# Définition des matchs
+
+def match(player1, player2, rounds, comments):
+    if comments : # Vérifie si l'on souhaite les commentaires des matchs
+        print("MATCH : Player", player1.name, "VERSUS Player", player2.name) # Print les infos du match
     for round in range(rounds) : # Répète pendant le nombre de tours souhaité
         outcome_player1, outcome_player2 = prisoner_dilemma(player1, player2, round) # Sauvegarde les résultats du tour en variable
         
         # Sauvegarde les coups de l'adversaire pour chaque joueur
         player1.memorize(player2.action)
         player2.memorize(player1.action)
-        
-        # Print les infos du tour
-        print("Round :", round + 1)
-        for player in ([player1, player2]): # Donne les actions pour les deux joueurs
-            print(player.name, "uses", player.chosenstrat.name)
-            if  player.action == 0 :
-                print(player.name, "cooperates !")
-            else :
-                print(player.name, "betrays !")
-        print("Previous scores :", score1, score2)
-        print("Gains :", outcome_player1, outcome_player2)
+        if comments : # Vérifie si l'on souhaite les commentaires du match
+            # Print les infos du tour
+            print("Round :", round + 1)
+            for player in ([player1, player2]): # Donne les actions pour les deux joueurs
+                print(player.name, "uses", player.chosenstrat.name)
+                if  player.action == 0 :
+                    print(player.name, "cooperates !")
+                else :
+                    print(player.name, "betrays !")
+            print("Previous scores :", player1.score, player2.score)
+            print("Gains :", outcome_player1, outcome_player2)
         
         # Effectuer réelement les changements des scores
-        score1 += outcome_player1
-        score2 += outcome_player2
-        
-        print("Present scores :", score1, score2) # Print les résultats totaux après le tour
+        player1.score += outcome_player1
+        player2.score += outcome_player2
+        if comments : # Vérifie si l'on souhaite les commentaires des matchs
+            print("Present scores :", player1.score, player2.score) # Print les résultats totaux après le tour
+    if comments : # Vérifie si l'on souhaite les commentaires des matchs
+        # Print les résultats finaux    
+        print("MATCH : Player", player1.name, "VERSUS Player", player2.name)   
+        print("FINAL RESULTS :", player1.score, player2.score)
     
-    # Print les résultats finaux    
-    print("MATCH : Player", player1.name, "VERSUS Player", player2.name)   
-    print("FINAL RESULTS :", score1, score2)
+# Définition des tournois
+
+def tournament(players, rounds, comments):
+    allplayers = players.copy() # Crée une liste de chaque joueurs, qu'ils aient joués ou non
+    while players : # Répéter tant qu'il reste des joueurs à ne pas avoir joués tout leurs matchs
+        for player2 in players : # Répéter pour chaque adversaire dans la liste des joueurs
+            match(players[0], player2, rounds, False) # Fait un match entre le premier joueur et son adversaire
+            if comments : # Vérifie si l'on souhaite les commentaires du tournoi
+                print("match entre", players[0].name, "et", player2.name)
+            # Ajoute les scores du matchs aux totaux
+            players[0].totalscore += players[0].score
+            player2.totalscore += player2.score
+            # Réinitialise les scores
+            players[0].nullifyscore()
+            player2.nullifyscore()
+        players.remove(players[0])# Supprime de la liste le joueur ayant fini tout ses matchs
+    allplayers = sorted(allplayers, key=lambda player: player.totalscore, reverse = True) # trie les joueurs par ordre décroissant des scores
+    if comments : # Vérifie si l'on souhaite les commentaires du tournoi
+        print("Classement par scores :")
+        for player in allplayers : # Donne le score de chaque joueur dans l'ordre de la liste triée
+            print(player.totalscore, player.name)
 
 # Définition de la classe des joueurs
 
@@ -66,6 +92,8 @@ class Player():
         self.name = name
         self.strategies = strategies
         self.memory = [] # Le joueur initie une liste vide comme mémoire des coups adverses passés
+        self.score = 0 # Le joueur démarre avec un score de 0
+        self.totalscore = 0 # Le joueur démarre avec un score total de 0
             
     def play(self, round):
         
@@ -83,8 +111,14 @@ class Player():
         
         return self.action
     
-    def memorize(self, move):
+    def memorize(self, move): # Ajoute un move à sa mémoire
         self.memory.append(move)
+        
+    def nullifyscore(self): # Reset son score à zéro
+        self.score = 0
+        
+    def nullifytotalscore(self): # Reset son score total à zéro
+        self.totalscore = 0
     
 # Définition des classes stratégies, le nom des classes sera sûrement changé à l'avenir pour correspondre aux noms originaux
 
@@ -186,8 +220,16 @@ davis = Stratdavis("Davis")
 
 Hoenn = Player("Hoenn", [tit_for_tat, tit_for_two_tat, roll_the_dice])
 Hisui = Player("Hisui", [no_redemption, tit_for_tat])
-DavisLover = Player("Davis Lover", [davis])       
+DavisLover = Player("Davis Lover", [davis])
+Allrandom = Player("All Random", [roll_the_dice])
+Ruben = Player("Ruben", [mean_change])
+Zahibra = Player("Zahibra", [no_redemption, roll_the_dice])
+Juliette = Player("Juliette", [davis, tit_for_tat])
 
 # Faire un match
 
-match(Hoenn, DavisLover, 20)
+# match(Hoenn, DavisLover, 20, True)
+
+# Faire un tournoi
+
+# tournament([DavisLover, Hisui, Hoenn, Allrandom, Ruben, Zahibra, Juliette], 20, True)
